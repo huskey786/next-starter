d6cbf75241e6e229e48b2e58c1b7ce037b20d36e
Khyam Hussain <khyam.hussain@rcni.com>
2022-12-08 15:37:03 +0000

    changed environment variables

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -1,11 +1,11 @@
 async function getAccessToken() {
-	const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
+	const refresh_token = process.env.NEXT_PUBLIC_SPOTIFY_REFRESH_TOKEN;
 
 	const response = await fetch('https://accounts.spotify.com/api/token', {
 		method: 'POST',
 		headers: {
 			Authorization: `Basic ${Buffer.from(
-				`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+				`${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID}:${process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET}`
 			).toString('base64')}`,
 			'Content-Type': 'application/x-www-form-urlencoded',
 		},
